af69a349384404c8fb5ac8e6708ee1537e1ed35a
parth prajapati <systemprocess06@gmail.com>
2026-08-14 23:09:37 +0530

    fix: correct terraform provider configuration syntax for helm kubernetes block

--- infra/terraform/k8s/main.tf
+++ infra/terraform/k8s/main.tf
@@ -21,10 +21,10 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.cluster_info.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_info.certificate_authority.0.data)
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
       command     = "aws"
