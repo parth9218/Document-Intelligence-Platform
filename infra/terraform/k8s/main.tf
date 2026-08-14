2bc655470eff41aa0d2df876137706f06670f539
parth prajapati <systemprocess06@gmail.com>
2026-08-14 23:15:27 +0530

    refactor: update EKS provider authentication to use data source token instead of exec plugin

--- infra/terraform/k8s/main.tf
+++ infra/terraform/k8s/main.tf
@@ -24,21 +24,12 @@ provider "helm" {
   kubernetes = {
     host                   = data.aws_eks_cluster.cluster_info.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_info.certificate_authority.0.data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster_info.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_info.certificate_authority.0.data)
-  load_config_file       = false
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    command     = "aws"
-  }
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
