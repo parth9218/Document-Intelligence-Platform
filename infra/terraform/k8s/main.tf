d180082aa45f35ec1da371cb75e87af29f8af900
parth prajapati <systemprocess06@gmail.com>
2026-08-14 23:06:01 +0530

    refactor: switch helm and kubernetes providers to use aws eks exec authentication instead of static tokens

--- infra/terraform/k8s/main.tf
+++ infra/terraform/k8s/main.tf
@@ -21,16 +21,24 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = data.aws_eks_cluster.cluster_info.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_info.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
   }
 }
 
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster_info.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_info.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
 }
