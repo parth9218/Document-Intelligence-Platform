7d8681085340ee8491f1bba48ea0a57e29a770f5
parth prajapati <systemprocess06@gmail.com>
2026-08-14 22:32:01 +0530

    chore: parameterize deployment directory and remove redundant backend config overrides in workflow and backend files

--- infra/terraform/k8s/backend.tf
+++ infra/terraform/k8s/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # These are dummy values to satisfy 'terraform validate'. They are overridden by -backend-config in the CI/CD pipeline.
-    bucket       = "bucket=tf-state-doc-intel-dev-389642461016-us-east-1-an"
+    bucket       = "tf-state-doc-intel-dev-389642461016-us-east-1-an"
     key          = "k8s/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
