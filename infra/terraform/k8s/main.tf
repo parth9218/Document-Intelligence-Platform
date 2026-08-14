3f0448b98223c7e241e3816b2624a47a298ad5b7
parth prajapati <systemprocess06@gmail.com>
2026-08-14 22:40:15 +0530

    refactor: remove unnecessary empty line from helm provider configuration

--- infra/terraform/k8s/main.tf
+++ infra/terraform/k8s/main.tf
@@ -19,7 +19,6 @@ data "aws_eks_cluster" "cluster_info" {
 data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_name
 }
-
 provider "helm" {
   kubernetes = {
     host                   = data.aws_eks_cluster.cluster_info.endpoint
