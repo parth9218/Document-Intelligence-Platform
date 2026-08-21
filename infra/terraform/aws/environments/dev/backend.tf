17b743197f64f17d4a56c5a1ce41a1b334c97889
parth prajapati <systemprocess06@gmail.com>
2026-08-21 19:48:23 +0530

    fix(infra): updated backend ref for terraform aws dev env, and passed necessary values to httproute api.

--- infra/terraform/aws/environments/dev/backend.tf
+++ infra/terraform/aws/environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tf-state-doc-intel-dev-389642461016-us-east-1-an"
-    key          = "aws/terraform.tfstate"
+    key          = "aws/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
