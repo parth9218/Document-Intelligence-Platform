942be89afb27b53f31ecb56b48817e3b77ed4c75
parth prajapati <systemprocess06@gmail.com>
2026-08-21 23:59:06 +0530

    fix(infra): changed default root object to index.html to serve the static SPA on cloudfront root.

--- infra/terraform/aws/modules/storage/main.tf
+++ infra/terraform/aws/modules/storage/main.tf
@@ -48,7 +48,21 @@ resource "random_uuid" "cf_origin_verify_token" {}
 resource "aws_cloudfront_distribution" "frontend" {
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = "/"
+  default_root_object = "index.html"
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
 
   # ── Origin 1: frontend S3 bucket ──────────────────────────────────────────
   origin {
